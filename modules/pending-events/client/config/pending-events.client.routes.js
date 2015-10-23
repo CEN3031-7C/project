31b816d575d9254ad130c44bd0efc726f5208c7b
muletmiles <muletmiles@ufl.edu>
2015-10-23 15:38:44 -0400

    Placed Pending Events link iside the Admin dropdown

--- modules/pending-events/client/config/pending-events.client.routes.js
+++ modules/pending-events/client/config/pending-events.client.routes.js
@@ -5,24 +5,24 @@ angular.module('pending-events').config(['$stateProvider',
 	function($stateProvider) {
 		// Pending events state routing
 		$stateProvider.
-		state('pending-events', {
+		state('admin.pending-events', {
 			abstract: true,
 			url: '/pending-events',
 			template: '<ui-view/>'
 		}).
-		state('pending-events.list', {
+		state('admin.pending-events.list', {
 			url: '',
 			templateUrl: 'modules/pending-events/client/views/events.client.view.html'//'modules/pending-events/views/list-pending-events.client.view.html'
 		}).
-		state('pending-events.create', {
+		state('admin.pending-events.create', {
 			url: '/create',
 			templateUrl: 'modules/pending-events/views/create-pending-event.client.view.html'
 		}).
-		state('pending-events.view', {
+		state('admin.pendin-events.view', {
 			url: '/:pendingEventId',
 			templateUrl: 'modules/pending-events/views/view-pending-event.client.view.html'
 		}).
-		state('pending-events.edit', {
+		state('admin.pending-events.edit', {
 			url: '/:pendingEventId/edit',
 			templateUrl: 'modules/pending-events/views/edit-pending-event.client.view.html'
 		});
